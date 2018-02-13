5d33d2e694d85ce8d83205093b86d5d5720e24f4
Lauri Gustafsson <luutifa@gmail.com>
2018-02-14 01:07:52 +0200

    Boilerplate, ditch dnload

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,112 @@
-#include "dnload.h"
+#include <SDL.h>
+#include <GL/glew.h>
+#include <iostream>
+#include <cstdlib>
 
-#if defined(USE_LD)
+#define BUFFER_W 1280
+#define BUFFER_H 720
+
+void toggle_fullscreen(SDL_Window *w) {
+  bool isFullscreen = SDL_GetWindowFlags(w) & SDL_WINDOW_FULLSCREEN;
+  SDL_SetWindowFullscreen(w, isFullscreen ? 0 : SDL_WINDOW_FULLSCREEN);
+  SDL_ShowCursor(isFullscreen); // Based on old value
+}
+
 int main(int argc, char *argv[]) {
-#else
-void _start() {
-#endif
+  int err;
 
-  dnload();
+  // Init SDL2
+  Uint32 const FLAGS = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS;
+  if ((err = SDL_Init(FLAGS)) < 0) {
+    std::cerr << "Failed to initialize SDL:"
+      << std::endl << SDL_GetError() << std::endl;
+    return err;
+  }
 
-  dnload_SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *w = dnload_SDL_CreateWindow(nullptr,
+  // Set OpenGL attributes
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+
+  // Init a window
+  SDL_Window *w = SDL_CreateWindow("instanssidemo",
       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-      1280, 720, SDL_WINDOW_SHOWN);
+      BUFFER_W, BUFFER_H, SDL_WINDOW_OPENGL);
 
-  /*SDL_Event e;
-  while (e.type != SDL_QUIT) {
-    dnload_SDL_PollEvent(&e);
-  }*/
+  if (w == nullptr) {
+    std::cerr << "Failed to get a window with sRGB:"
+      << std::endl << SDL_GetError() << std::endl;
 
-#if defined(USE_LD)
+    // Try again without sRGB
+    SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 0);
+    w = SDL_CreateWindow("instanssidemo",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        BUFFER_W, BUFFER_H, SDL_WINDOW_OPENGL);
+
+    if (w == nullptr) { // Still no window?
+      std::cerr << "Failed to get a window without sRGB (exiting):"
+        << std::endl << SDL_GetError() << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  // Init a context
+  SDL_GLContext c = SDL_GL_CreateContext(w);
+  if (c == nullptr) {
+    std::cerr << "Failed to get an OpenGL context:"
+      << std::endl << SDL_GetError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Load OpenGL driver/libraries/api/calls/whatever
+  if ((err = glewInit()) != GLEW_OK) {
+    std::cerr << "GLEW failed to initialize:"
+      << std::endl << glewGetErrorString(err) << std::endl;
+    return err;
+  }
+
+  // Check if OpenGL framebuffer got SRGB capability and enable it
+  int framebuffer_srgb;
+  SDL_GL_GetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, &framebuffer_srgb);
+  if (framebuffer_srgb) {
+    glEnable(GL_FRAMEBUFFER_SRGB);
+  }
+
+  // Set OpenGL up further
+  glViewport(0, 0, BUFFER_W, BUFFER_H);
+  //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glClearColor(1, 0, 0, 1); // Red for visibility
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Demo loop
+  SDL_Event e;
+  bool running = true;
+  while (running) {
+    // Swap window framebuffers
+    SDL_GL_SwapWindow(w);
+
+    // Handle SDL events such as kill signals and keyboard
+    SDL_PollEvent(&e);
+    switch(e.type) {
+      case SDL_QUIT:
+        running = false;
+        break;
+      case SDL_KEYDOWN:
+        switch (e.key.keysym.sym) {
+          case SDLK_q:
+          case SDLK_ESCAPE:
+            running = false;
+            break;
+          case SDLK_f:
+            toggle_fullscreen(w);
+            break;
+        }
+        break;
+    }
+  }
+
   SDL_Quit();
   return 0;
-#else
-  asm_exit();
-#endif
 }
