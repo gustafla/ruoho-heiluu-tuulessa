d0db766751d20e36d58c06b84436f7a92907c1be
Lauri Gustafsson <luutifa@gmail.com>
2018-02-26 17:13:26 +0200

    CMake copy shader sources

--- src/grass.cpp
+++ src/grass.cpp
@@ -14,7 +14,10 @@ Grass::Grass(Demo &demo): demo(demo) {
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(v), v, GL_STATIC_DRAW);
 
-
+  program = linkProgram(loadFile("vertex.glsl"), loadFile("fragment.glsl"));
+  if (!program) {
+    die(EXIT_FAILURE);
+  }
 }
 
 void Grass::render() {
